d8065310b1ae791bdc8cc933b1a3f94972ca062b
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2019-08-01 10:23:21 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,9 @@
 # Configure the IBM Cloud Provider
 provider "ibm" {
-  bluemix_api_key    = "${var.ibm_bx_api_key}"
-  softlayer_username = "${var.ibm_sl_username}"
-  softlayer_api_key  = "${var.ibm_sl_api_key}"
+  ibmcloud_api_key    = "${var.ibmcloud_api_key}"
+  softlayer_username = "${var.softlayer_username}"
+  softlayer_api_key  = "${var.softlayer_api_key}"
+  generation = 1
+  region = "us-south"
 }
 
